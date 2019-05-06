ce7aebf2ccecb362b5c80fa0ec96f520e2489ad3
Sean Young <seany@Seans-MBP.local>
2019-05-06 00:30:45 -0700

    blockwiener

--- blockwiener.m
+++ blockwiener.m
@@ -1,10 +1,13 @@
-function u = blockwiener(u, Hx, Hy, Hz, lambda)
+function [ux, uy, uz] = blockwiener(tx, ty, tz, Hx, Hy, Hz, lambda)
+    ux = tx;
+    uy = ty - Lyx*ux;
+    uz = tz - Lzx*ux - Lzy*uy;
     
+    ux = Dxx*ux;
+    uy = Dyy*uy;
+    uz = Dzz*uz;
 
-
-
-
-
-
-
+    uz = uz;
+    uy = uy - Lyz*uz;
+    ux = ux - Lxy*uy - Lxz*uz;
 end
