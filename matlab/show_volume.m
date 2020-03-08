de5fcd8e1effe033a210166c6e7f1f35b21226c3
Sean Young <seannz@gmail.com>
2020-03-07 20:57:55 -0800

    remove unnecessary files

--- matlab/show_volume.m
+++
@@ -1,7 +0,0 @@
-function show_volume(dump_file)
-% show_volume loads the given dump_file and shows it on matlab's volume viewer 
-% After that you can adjust the visualization parameters to see the hidden geometry
-
-    vol = load_dump(dump_file);
-    volumeViewer(vol);
-end
